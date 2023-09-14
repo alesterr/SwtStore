06f0d70912dcbbcd1ec449cb0dfe85d677ec2708
AlesterR <alesterrods@gmail.com>
2023-09-14 17:06:56 +0400

    final

---
+++ components/navbar.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+
+import MainNav from "@/components/main-nav";
+import Container from "@/components/ui/container";
+import NavbarActions from "@/components/navbar-actions";
+import getCategories from "@/actions/get-categories";
+import {ThemeToggle} from "@/components/theme-toggle"
+import { Image } from "lucide-react";
+
+const Navbar = async () => {
+  const categories = await getCategories();
+
+  return ( 
+    <div className="border-b">
+      <Container>
+        <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
+          <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
+            <p className="font-bold text-xl">
+              <img
+              src="/swtlogo.png"
+              width={50}
+              height={50}
+              /> 
+            </p>
+          </Link>
+          <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
+            <p className="font-bold text-xl">
+              STYLE WALA TRENDS
+            </p>
+          </Link>
+          {/* <ThemeToggle /> */}
+          <MainNav data={categories} />
+          <NavbarActions />
+        </div>
+      </Container>
+    </div>
+  );
+};
+ 
+export default Navbar;
